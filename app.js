1f920827b9dc7968c6b2b7fd597c8167263f153d
Hassaan Mahmood <hassaan2980@gmail.com>
2026-06-27 13:07:13 -0700

    Loop Tasks Completed

--- app.js
+++ app.js
@@ -1,209 +1,147 @@
-// var number = [1, 2, 3, 4, 5, 6, 7]
-// for (var i = 0; i < number.length; i++) {
-//    console.log(number[i])
-// }
+/* ================== Loops ================== */
 
-// Task 1
+// Loop executed reverse order
+ for (var i = 10; i >= 0; i--){
+    console.log(i);
+ }
 
-// var numbers = [12, 4, -8, -12, -25, 5, 5, 3, -9, 7, -5, -7]
-// var negetiveNum = 0
+Loop run in forward order
+var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < numbers.length; i++){
+    console.log(numbers[i]);
+}
 
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] < negetiveNum) {
-//       negetiveNum++
-//    }
-// }
+// ******** Negetive Number Count ******** //
+var numbers = [1, -2, 3, -4, 5, -6, 7, 8, -9, 10];
+var count = 0;
 
-// console.log(negetiveNum)
+for (var i = 0; i < numbers.length; i++){
+    var num = numbers[i];
+    if (num < 0) {
+       count++;
+    // console.log(num);
+}
+}
+console.log(count);
 
-// Task 2 Even & Odd
+// ******** Even and Odd Seprated Through loop ******** //
+let numbers = [10, 11, 12, 13,14, 15, 16, 17, 18, 19, 20];
+let evenNumbers = [];
+let oddNumbers = [];
 
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
-// var evenNum = [];
-// var oddNum = [];
+for (let i = 0; i < numbers.length; i++){
+    let num = numbers[i];
+    if (num % 2 === 0){
+        evenNumbers.push(num);
+    } else {
+        oddNumbers.push(num);
+    }
+}
 
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] % 2 === 0) {
-//       evenNum.push(numbers[i])
-//    } else {
-//       oddNum.push(numbers[i])
-//    }
-// }
+// console.log("Even Numbers: ", evenNumbers);
+console.log("Odd Numbers: ", oddNumbers);
 
-// console.log(evenNum)
-// console.log(oddNum)
+******** Largest Number Find ******** //
+let numbers = [11, 22, 33, 44, 55, 66, 77];
+let largestNumber = numbers[0];
 
-// Task 3 Sum of all numbers of array
+for (let i = 0; i < numbers.length; i++) {
+  let num = numbers[i];
+  if (num > largestNumber) {
+    largestNumber = num;
+  }
+}
 
-// var numbers = [1, 2, 3, 4, 5, 6]
-// var sum = 0;
+console.log(largestNumber);
 
-// for (var i = 0; i < numbers.length; i++) {
-//    sum += numbers[i]
-// }
+// ******** Smallest Largest Number Find ******** //
+let numbers = [11, 22, 33, 44, 55, 66, 77];
+let smallestNumber = numbers[0];
 
-// console.log(sum)
+for (let i = 0; i < numbers.length; i++) {
+  let num = numbers[i];
+  if (num < smallestNumber) {
+    smallestNumber = num;
+  }
+}
 
-// Task 4 Greater Number
+// console.log(smallestNumber);
 
-// var greaterNum = 0;
+******** Second Largest Number Find ******** //
+let numbers = [11, 22, 33, 44, 55, 66, 77, 88];
+let largestNum = numbers[0];
+let secLargNum = numbers[0];
 
-// var numbers = [1, 2, 3, 4, 5, 6]
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] > greaterNum) {
-//       greaterNum = numbers[i]
-//    }
-// }
-// console.log(greaterNum)
+for (let i = 0; i < numbers.length; i++) {
+  let num = numbers[i];
+  if (num > largestNum) {
+    largestNum = num;
+  }
+}
 
-// Task 5 Smallest Number
+for (let i = 0; i < numbers.length; i++) {
+  let num = numbers[i];
+  if (num > secLargNum && num < largestNum) {
+    secLargNum = num;
+  }
+}
 
-// var smallestNum = 0;
+// console.log(largestNum);
+// console.log(secLargNum);
 
-// var numbers = [-1, -2, 3, -4, 5, -6]
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] < smallestNum) {
-//       smallestNum = numbers[i]
-//    }
-// }
-// console.log(smallestNum)
+// ******** Nested Loop ******** //
+let firstNames = ["Muhammad", "Ali"];
+let lastNames = ["Hassan", "Ahmed", "Ajmal", "Umar", "ZUlfiqar"];
 
-// *Bug in this way
-// var smallestNum = 0;
-
-// var numbers = [1, 2, 3, 4, 5, 6]
-// for (var i = 0; i < numbers.length; i++) {
-
-// condition failed
-//    if (numbers[i] < smallestNum) {
-//       smallestNum = numbers[i]
-//    }
-// }
-// console.log(smallestNum)
-
-// *Correct
-// Find the smallest number form the array
-// var smallestNum = numbers[0];
-
-// var numbers = [67, 34, 44, 33, 12];
-
-// var smallestNum = numbers[0];
-
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] < smallestNum) {
-//       smallestNum = numbers[i];
-//    }
-// }
-
-// console.log(smallestNum)
-
-
-
+for (let a = 0; a < firstNames.length; a++) {
+  let name1 = firstNames[a];
+  for (let b = 0; b < lastNames.length; b++) {
+    let name2 = lastNames[b];
+    console.log(name1, name2);
+  }
+}
 
 
-// Task 6
-// Second Largest Number
+// ******** Dublicate Number Find ******** //
+let numbers = [1, 2, 3, 4, 4, 5, 6, 6];
+let dubNum = [];
+for (let i = 0; i < numbers.length; i++) {
+    let prefix = numbers[i];
+    for (let j = i + 1; j < numbers.length; j++){
+        let suffix = numbers[j];
+        if (prefix === suffix){
+            dubNum.push(prefix);
+        }
+    }
 
-// var helperNum = 0;
-// var secondLastNum = 0;
 
-// var numbers = [10, 20, 30, 40, 50, 60, 77, 99, 120, 250, 39]
-
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] > helperNum) {
-//       helperNum = numbers[i];
-//    }
-// }
-
-// var index = numbers.indexOf(helperNum)
-// if (index > -1) {
-//    numbers.splice(index, 1)
-// }
-
-// for (var i = 0; i < numbers.length; i++) {
-//    if (numbers[i] > secondLastNum) {
-//       secondLastNum = numbers[i];
-//    }
-// }
-
-// console.log(helperNum);
-// console.log(secondLastNum);
+}
+console.log(dubNum);
 
 
 
-// Task 7
-// Dublicate Number
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 2, 3, 4];
-// var count = 0;
 
-// for (var i = 0; i < numbers.length; i++) {
-//     for (var z = i + 1; z < numbers.length; z++) {
-//         if (numbers[i] === numbers[z]) {
-//             // console.log(numbers[i]);
-//             count += numbers[i].length;
-//             break;
-//         }
-//     }
-// }
-
-// console.log(count)
-
-
-// In Class 
-// Largest Number 
-// var numbers = [1, 2, 3, 4, 5, 6, 7, 9, 10]
-
-// var LargestNumber = numbers[0];
-// var secondLargestNumber = numbers[0];
+var userWord = prompt("Enter your word")
+var word = "mom";
+var reverseWord = "";
 
 
 
-// for (var i = 0; i < numbers.length; i++) {
-//     var iteration = numbers[i];
-//     if (iteration > LargestNumber){
-//         LargestNumber = iteration;
-//     }
-//  }
-
-//  for (var i = 0; i < numbers.length; i++) {
-//     var iteration = numbers[i];
-//     if (iteration > secondLargestNumber && iteration < LargestNumber){
-//         secondLargestNumber = iteration;
-//     }
-//  }
-
-//  console.log(LargestNumber);
-//  console.log(secondLargestNumber);
+for (var i = word.length - 1; i >= 0; i--){
+    var iteration = word[i];
+    reverseWord = reverseWord + iteration;
+}
 
 
-// Nested Loop 
-// var firstName = ["Muhammad", "Ali"];
-// var lastName = ["Hamza", "Kamran", "Luqman", "Bilal"];
+// console.log(reverseWord)
 
-// for (var i = 0; i < firstName.length; i++) {
-//     var prefix = firstName[i];
-//     for (var j = 0; j < lastName.length; j++) {
-//         var suffix = lastName[j];
-//         console.log(prefix, suffix);
-//     }
-// }
+if (word === reverseWord){
+    console.log("It is a Palindrome")
+   
+} else {
+        console.log("It is not a Palindrome")
 
-
-
-// HardTask 
-
-// var numbers = [1, 2, 3, 1, 2];
-// var dublicateNum = 0;
-
-// for (var i = 0; i < numbers.length; i++) {
-//     var prefix = numbers[i];
-//     for (var j = i + 1; j < numbers.length; j++) {
-//         var suffix = numbers[j];
-//         if (prefix === suffix) {
-//             console.log(prefix)
-//         }
-//     }
-// }
+    }
 
 
 
